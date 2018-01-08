22cae8b91d133267ee3d562482e677ff40b02a48
joshuasellers <joshuasellersd@gmail.com>
2018-01-07 21:43:10 -0700

    ui
    working on ui for song-based selections

--- Music/src/musicdb/MusicDBMain.java
+++ Music/src/musicdb/MusicDBMain.java
@@ -13,6 +13,29 @@ import java.util.Arrays;
  * Created by joshuasellers on 6/5/17.
  */
 public class MusicDBMain {
+    public static void song(DB db, Scanner scan){
+        System.out.println("Type in keywords:");
+        String s = scan.next();
+        String song = SongTable.printPossibleSongs(db.getConnection(), s, scan);
+        //while (scan.hasNext()) inp += " " +scan.next();
+        if (!song.equals("q")) {
+            System.out.println("Length of playlist:");
+            int l = Integer.parseInt(scan.next());
+            SongTable.createPlaylist(db.getConnection(),song,l);
+        }
+
+    }
+    public static void artist(DB db, Scanner scan){
+
+    }
+    public static void genre(DB db, Scanner scan){
+
+    }
+    public static void mood(DB db, Scanner scan){
+
+    }
+
+
     /**
      * @param args the command line arguments
      */
@@ -35,13 +58,21 @@ public class MusicDBMain {
             System.out.printf("%32s\n", "THE PLAYLIST MAKER");
             System.out.println(String.join("", Collections.nCopies(50, "*")));
             System.out.println("Use these commands to run queries on the data:");
-            System.out.println("  p: explore player data");
-            System.out.println("  t: explore team data");
-            System.out.println("  d: explore division data");
-            System.out.println("  c: explore conference data");
+            System.out.println("  s: playlist based on song");
+            System.out.println("  a: playlist based on artist");
+            System.out.println("  g: playlist based on genre");
+            System.out.println("  m: playlist based on mood");
             System.out.println("  q: quit database");
             String s = scan.next();
             if(s.equals("q")){running = false;}
+            else if (s.equals("s")) {song(db, scan);}
+            else if (s.equals("a")) {artist(db, scan);}
+            else if (s.equals("g")) {genre(db, scan);}
+            else if (s.equals("m")) {mood(db, scan);}
+            else {
+                System.out.println("ERROR");
+                running = false;
+            }
         }
         db.closeConnection();
     }
