b56ab22288429e407c3f2e46c5d5eabb30f61582
joshuasellers <joshuasellersd@gmail.com>
2018-07-25 21:55:51 -0400

    made methods

--- MileSplits/MileSplits.c
+++ MileSplits/MileSplits.c
@@ -18,6 +18,13 @@
 #define tenk 6.2
 #define fivek 3.1
 
+float Race_Time(float dist, float split){
+  return split * dist;
+}
+
+float Race_Split(float dist, float time){
+  return dist/time;
+}
 
 int main(int argc, char const *argv[]) {
 	/*
@@ -26,10 +33,10 @@ int main(int argc, char const *argv[]) {
 	*/
   if(argc == 3){
     if (strcmp(argv[0],"split") == 0) {
-      
+      printf("%f\n", Race_Split(atof(argv[1]),atof(argv[2]));
     }
     else if (strcmp(argv[0],"time") == 0){
-
+      printf("%f\n", Race_Time(atof(argv[1]),atof(argv[2])));
     }
     else{
       return EXIT_FAILURE;
