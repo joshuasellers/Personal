4683f5c25ec2b824ee2f8689473fb2202c9b4f9a
joshuasellers <joshuasellersd@gmail.com>
2018-07-28 16:52:41 -0400

    conversion is annoying. need to test it

--- MileSplits/MileSplits.c
+++ MileSplits/MileSplits.c
@@ -17,14 +17,44 @@
 #define half 13.1
 #define tenk 6.2
 #define fivek 3.1
+#define MAX 100
 
-float Race_Time(float dist, float split){
-  int seconds =
-  return (split * dist);
+float string_convert(char* arg){
+  int i = 0;
+  colon = 0;
+  while (arg[i] != '\0') {
+    if (arg[i] == ':') {
+      colon = i;
+    }
+    i++;
+  }
+  char* min;
+  min = (char*)malloc(colon*sizeof(char));
+  for (size_t j = 0; j < colon; j++) {
+    min[j] = arg[j];
+  }
+  char* sec;
+  sec = (char*)malloc((i-colon)*sizeof(char));
+  for (size_t k = colon+1; k <= i; k++) {
+    sec[k] = arg[k];
+  }
+  return (atof(min)+(atof(sec)/60.0));
 }
 
-float Race_Split(float dist, float time){
-  return (time/dist);
+char* time_convert(float t){
+  char buf[MAX];
+  gcvt(t, 6, buf);
+  return buf
+}
+
+char* Race_Time(float dist, char* split){
+  float split_correct = string_convert(split);
+  return time_convert(split_correct * dist);
+}
+
+char* Race_Split(float dist, char* time){
+  float time_correct = string_convert(time);
+  return time_convert(time/dist);
 }
 
 int main(int argc, char const *argv[]) {
@@ -36,10 +66,10 @@ int main(int argc, char const *argv[]) {
   printf("argc = %d\n", argc);
   if(argc == 4){
     if (strcmp(argv[0],"split")) {
-      printf("Your mile split for %s miles over %s minutes is %.2f\n", argv[2],argv[3], Race_Split(atof(argv[2]),atof(argv[3])));
+      printf("Your mile split for %s miles over %s minutes is %s\n", argv[2],argv[3], Race_Split(atof(argv[2]),argv[3]));
     }
     else if (strcmp(argv[0],"time")){
-      printf("Your time for %s miles with a split of %s is %.2f\n", argv[2],argv[3], Race_Time(atof(argv[2]),atof(argv[3])));
+      printf("Your time for %s miles with a split of %s is %s\n", argv[2],argv[3], Race_Time(atof(argv[2]),argv[3]));
     }
     else{
       return EXIT_FAILURE;
