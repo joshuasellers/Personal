4e537ceed38137c40a2575d6dd4bb83d4c883ff1
joshuasellers <joshuasellersd@gmail.com>
2018-07-29 22:15:57 -0400

    just need to check everything and comment

--- MileSplits/MileSplits.c
+++ MileSplits/MileSplits.c
@@ -1,7 +1,7 @@
 ///
 /// File: ahnentafel.c
 ///
-/// A program to get mile splits.
+/// A program for race times.
 ///
 ///
 /// @author Josh Sellers
@@ -19,42 +19,66 @@
 #define fivek 3.1
 #define MAX 100
 
-float string_convert(char* arg){
+float string_convert(const char* arg){
   int i = 0;
-  colon = 0;
+  int colon = 0;
   while (arg[i] != '\0') {
-    if (arg[i] == ':') {
-      colon = i;
-    }
+    if (arg[i] == ':') {colon = i;}
     i++;
   }
-  char* min;
-  min = (char*)malloc(colon*sizeof(char));
-  for (size_t j = 0; j < colon; j++) {
-    min[j] = arg[j];
-  }
-  char* sec;
-  sec = (char*)malloc((i-colon)*sizeof(char));
-  for (size_t k = colon+1; k <= i; k++) {
-    sec[k] = arg[k];
-  }
-  return (atof(min)+(atof(sec)/60.0));
+  if (colon == 0) {colon = i;}
+  char* holder;
+  char* min_str;
+  min_str = (char*)malloc((colon+1)*sizeof(char));
+  for (size_t j = 0; j < colon; j++) {min_str[j] = arg[j];}
+  min_str[colon] = '\0';
+  long min_f = strtol(min_str, &holder, 10);
+  char* sec_str;
+  sec_str = (char*)malloc((i-colon)*sizeof(char));
+  for (size_t k = 0; k < 2; k++) {sec_str[k] = arg[k+colon+1];}
+  sec_str[3] = '\0';
+  long sec_f = strtol(sec_str, &holder, 10);
+  float correct_time = min_f +(sec_f/60.0);
+  return correct_time;
 }
 
 char* time_convert(float t){
-  char buf[MAX];
-  gcvt(t, 6, buf);
-  return buf
+  char* time_str = (char*)malloc(MAX*sizeof(char));
+  gcvt(t, 6, time_str);
+  int i = 0;
+  int period = 0;
+  while (time_str[i] != '\0') {
+    if (time_str[i] == '.') {period = i;}
+    i++;
+  }
+  if (period == 0) {period = i;}
+  char* holder;
+  char* min_str;
+  min_str = (char*)malloc((period+1)*sizeof(char));
+  for (size_t j = 0; j < period; j++) {min_str[j] = time_str[j];}
+  min_str[period] = '\0';
+  char* sec_str;
+  sec_str = (char*)malloc((i-period+1)*sizeof(char));
+  for (size_t k = period; k < i; k++) {sec_str[k-period] = time_str[k];}
+  sec_str[i-period] = '\0';
+  float sec_f = atof(sec_str)*60.0;
+  char* final_sec = (char*)malloc(MAX*sizeof(char));
+  gcvt(sec_f, 4, final_sec);
+  char *result = malloc(strlen(min_str) + strlen(final_sec) + 2);
+  strcpy(result, min_str);
+  strcat(result, ":");
+  strcat(result, final_sec);
+  return result;
 }
 
-char* Race_Time(float dist, char* split){
+char* Race_Time(float dist, const char* split){
   float split_correct = string_convert(split);
   return time_convert(split_correct * dist);
 }
 
-char* Race_Split(float dist, char* time){
+char* Race_Split(float dist, const char* time){
   float time_correct = string_convert(time);
-  return time_convert(time/dist);
+  return time_convert(time_correct/dist);
 }
 
 int main(int argc, char const *argv[]) {
@@ -63,12 +87,11 @@ int main(int argc, char const *argv[]) {
   splits or times depending on the input.
 	*/
   //TODO make strings for the argvs and interate through them
-  printf("argc = %d\n", argc);
   if(argc == 4){
-    if (strcmp(argv[0],"split")) {
+    if (strcmp(argv[1],"split") == 0) {
       printf("Your mile split for %s miles over %s minutes is %s\n", argv[2],argv[3], Race_Split(atof(argv[2]),argv[3]));
     }
-    else if (strcmp(argv[0],"time")){
+    else if (strcmp(argv[1],"time") == 0){
       printf("Your time for %s miles with a split of %s is %s\n", argv[2],argv[3], Race_Time(atof(argv[2]),argv[3]));
     }
     else{
@@ -76,8 +99,8 @@ int main(int argc, char const *argv[]) {
     }
   }
   else{
+    printf("Incorrect Number of args\n");
     return EXIT_FAILURE;
   }
-  printf("Incorrect Number of args\n");
 	return EXIT_SUCCESS;
 }
