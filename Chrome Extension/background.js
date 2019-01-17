71394c0fc27e18f88a2908e2e5fdda3e004ac3fa
Josh Sellers <joshuasellersd@gmail.com>
2019-01-16 21:12:41 -0500

    Tutorial Complete
    Completed the tutorial. I will try to modify the code this week.

--- Chrome Extension/background.js
+++ Chrome Extension/background.js
@@ -1,7 +1,15 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({time: 0}, function() {
-      console.log("The time is set to 0.");
-    });
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log('The color is green.');
   });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'developer.chrome.com'},
+      })],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
