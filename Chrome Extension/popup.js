80dbd51477b27ec44763d411a8008cc9b331bd6b
Josh Sellers <joshuasellersd@gmail.com>
2019-02-07 17:02:37 -0500

    debugged some errors
    in popup.js

--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -1,12 +1,18 @@
 'use strict';
 
-chrome.tabs.query(null,function(tab) {
-	chrome.extension.getBackgroundPage().timers[tabId] = new Date();
+chrome.tabs.query(
+			{
+			    active: true,               // Select active tabs
+			    lastFocusedWindow: true     // In the current window
+			}, function(tabs) {
+	var tabID = tabs[0].id
+	var url = tabs[0].url;
+	chrome.extension.getBackgroundPage().timers[tabID] = new Date();
 	var currentTime = new Date();
-	if(currentTime - chrome.extension.getBackgroundPage().timers[tabId] > 5000)
+	if(currentTime - chrome.extension.getBackgroundPage().timers[tabID] > 5000)
 	{
 	  chrome.browserAction.enable(tabId);
-	  chrome.browserAction.setIcon({path: "icon-enabled.png", tabId: tabId});
+	  chrome.browserAction.setIcon({path: "icon-enabled.png", tabID: tabID});
 	}
-}
+});
 
