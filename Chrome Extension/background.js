7deb0bbefe715ab9fdc0cc9a126773ee0acd74db
Josh Sellers <joshuasellersd@gmail.com>
2019-02-14 13:26:16 -0500

    Removed redundent method
    onCreated doesn't seem to really matter

--- Chrome Extension/background.js
+++ Chrome Extension/background.js
@@ -2,33 +2,49 @@
 
 
 var timers = [];
-
+var inYouTube = false;
+var curr = 0;
+/*
 chrome.tabs.onCreated.addListener(
 function(tab) 
 {
-	var tabIDC = tab.id;
-	var urlC = tab.url;
-	console.log("new tab");
-	if (urlC.includes("youtube.com")) {
-		console.log("youtube!");
-		timers[tabIDC] = new Date();
-		var time = FormatTime(timers)
+	var tabID = tab.id;
+	var url = tab.url;
+	if (inYouTube){
+		console.log("Get time for new tab");
+		timers[curr] = new Date() - timers[curr];
+		var time = GetTime(timers)
 		console.log(time);
+		var format = FormatTime(time);
+		console.log(format)
+	}
+	inYouTube = false;
+	if (url.includes("youtube.com")) {
+		console.log("new youtube tab!");
+		inYouTube = true;
+		curr = tabID;
+		timers[tabID] = new Date();
 	}
 });
-
+*/
 chrome.tabs.onUpdated.addListener(
 function(tabId, changeInfo, tab) 
 {
 	var tabID = tabId;
 	var url = changeInfo.url;
-	if (url != null && url.includes("youtube.com")) {
-		console.log("youtube!");
-		console.log(url);
-		timers[tabID] = new Date();
+	if (inYouTube){
+		console.log("Get time for updated");
+		timers[curr] = new Date() - timers[curr];
 		var time = GetTime(timers)
 		console.log(time);
 		var format = FormatTime(time);
 		console.log(format)
+	}
+	inYouTube = false;
+	if (url != null && url.includes("youtube.com")) {
+		inYouTube = true;
+		console.log("updated to youtube!");
+		console.log(url);
+		timers[tabID] = new Date();
 	};
 });
