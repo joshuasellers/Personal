47d83cc851cfd26480b949683457111a3015827e
Josh Sellers <joshuasellersd@gmail.com>
2019-02-15 11:46:02 -0500

    kinda have a log, too small tho

--- Chrome Extension/background.js
+++ Chrome Extension/background.js
@@ -32,3 +32,16 @@ function(tabId, changeInfo, tab)
 		}
 	;}
 });
+
+function updateTime(){
+	if (inYouTube){
+			console.log("Get time for updated");
+			var holder = timers[curr];
+			timers[curr] = new Date() - timers[curr];
+			current_time = GetTime(timers);
+			console.log(current_time);
+			current_format = FormatTime(current_time);
+			console.log(current_format);
+			timers[curr] = holder;
+		}
+}
--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -7,8 +7,7 @@ resetTime.onclick = function() {
 	chrome.extension.getBackgroundPage().inYouTube = false;
 	chrome.extension.getBackgroundPage().curr = 0;
 	chrome.extension.getBackgroundPage().current_timer = 0;
-	chrome.extension.getBackgroundPage().current_format = "";
-	console.log("reset")
+	chrome.extension.getBackgroundPage().current_format = "00:00";
 };
 
 let getTime = document.getElementById('getTime');
