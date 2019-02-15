aaf859fc48539ea3c1f37f6e68997c764f0c9f1f
Josh Sellers <joshuasellersd@gmail.com>
2019-02-15 16:23:24 -0500

    Working timer!!!!

--- Chrome Extension/background.js
+++ Chrome Extension/background.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 var timers = [];
 var inYouTube = false;
 var curr = 0;
@@ -8,40 +7,44 @@ var current_time = 0;
 var current_format = "00:00";
 
 chrome.tabs.onUpdated.addListener(
-function(tabId, changeInfo, tab) 
-{
-	if (changeInfo.status == "complete"){
-		var tabID = tabId;
-		var url = tab.url;
-		console.log("function")
-		if (inYouTube){
-			console.log("Get time for updated");
-			timers[curr] = new Date() - timers[curr];
-			current_time = GetTime(timers);
-			console.log(current_time);
-			current_format = FormatTime(current_time);
-			console.log(current_format);
-			inYouTube = false;
-		}
-		if (url != null && url.includes("youtube.com")) {
-			inYouTube = true;
-			console.log("updated to youtube!");
-			console.log(url);
-			timers[tabID] = new Date();
-			curr = tabID;
-		}
-	;}
-});
+	function(tabId, changeInfo, tab) 
+	{
+		if (changeInfo.status == "complete"){
+			var tabID = tabId;
+			var url = tab.url;
+			console.log("function");
+			if (inYouTube){
+				console.log("Get time for updated");
+				timers[curr] = new Date() - timers[curr];
+				current_time = GetTime(timers);
+				console.log(current_time);
+				current_format = FormatTime(current_time);
+				console.log(current_format);
+				inYouTube = false;
+			}
+			if (url != null && url.includes("youtube.com")) {
+				inYouTube = true;
+				console.log("updated to youtube!");
+				console.log(url);
+				timers[tabID] = new Date();
+				curr = tabID;
+			}
+		;}
+	});
 
 function updateTime(){
 	if (inYouTube){
-			console.log("Get time for updated");
-			var holder = timers[curr];
-			timers[curr] = new Date() - timers[curr];
-			current_time = GetTime(timers);
-			console.log(current_time);
-			current_format = FormatTime(current_time);
-			console.log(current_format);
-			timers[curr] = holder;
-		}
+		console.log("Get time for updated");
+		var holder = timers[curr];
+		timers[curr] = new Date() - timers[curr];
+		current_time = GetTime(timers);
+		console.log(current_time);
+		current_format = FormatTime(current_time);
+		console.log(current_format);
+		timers[curr] = holder;
+		return current_format;
+	}
+	else{
+		return current_format;
+	}
 }
--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -10,9 +10,35 @@ resetTime.onclick = function() {
 	chrome.extension.getBackgroundPage().current_format = "00:00";
 };
 
-let getTime = document.getElementById('getTime');
+var bgPage = chrome.extension.getBackgroundPage();
+var myVar = setInterval(myTimer, 1000);
 
-getTime.onclick = function() {
-	var time = chrome.extension.getBackgroundPage().current_format;
-	chrome.browserAction.setBadgeText({text: time});
-};
+function myTimer() {
+	var yt = chrome.extension.getBackgroundPage().inYouTube;
+	if (yt){
+		document.getElementById("clock").textContent = bgPage.updateTime();
+		var time = chrome.extension.getBackgroundPage().current_format;
+		chrome.browserAction.setBadgeText({text: time});
+	}
+	else{
+		document.getElementById("clock").textContent = chrome.extension.getBackgroundPage().current_format;
+		var time = chrome.extension.getBackgroundPage().current_format;
+		chrome.browserAction.setBadgeText({text: time});
+	}
+}
+
+
+
+/*
+var yt = chrome.extension.getBackgroundPage().inYouTube;
+var currentTime = new Date();
+while (yt){
+	if(new Date() - currentTime > 1000)
+	{
+	  	let clock = document.getElementById('clock');
+		clock.textContent = bgPage.updateTime();
+	}
+	currentTime = new Date();
+	yt = chrome.extension.getBackgroundPage().inYouTube;
+}
+*/
