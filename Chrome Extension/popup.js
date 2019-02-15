a8ead833a730fe46b788ecfa70e40467733a5836
Josh Sellers <joshuasellersd@gmail.com>
2019-02-14 22:26:09 -0500

    working on ui

--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -1,13 +1,17 @@
 'use strict';
 
-chrome.tabs.query(
-			{
-			    active: true,               // Select active tabs
-			    lastFocusedWindow: true     // In the current window
-			}, function(tabs) {
-	var tabID = tabs[0].id;
-	var url = tabs[0].url;
-	chrome.extension.getBackgroundPage().timers[tabID] = new Date();
-	var currentTime = new Date();
-});
+let resetTime = document.getElementById('resetTimer');
 
+resetTime.onclick = function() {
+  chrome.extension.getBackgroundPage().timers = [];
+  chrome.extension.getBackgroundPage().inYouTube = false;
+  chrome.extension.getBackgroundPage().curr = 0;
+};
+
+let getTime = document.getElementById('getTimer');
+
+getTime.onclick = function() {
+  chrome.extension.getBackgroundPage().timers = [];
+  chrome.extension.getBackgroundPage().inYouTube = false;
+  chrome.extension.getBackgroundPage().curr = 0;
+};
