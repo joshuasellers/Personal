4f4f3b7a5b330b4187283db49c1f687d0db794d8
Josh Sellers <joshuasellersd@gmail.com>
2019-02-27 22:39:17 -0500

    made blank functions

--- Haskell DSL/CardGameDSL.hs
+++ Haskell DSL/CardGameDSL.hs
@@ -28,12 +28,51 @@ draw _ _ = []
 discard :: [Card] -> Integer -> [Card]
 discard _ _ = []
 
--- shuffel there looks like a decent shuffle io for haskell online
+-- shuffel: there looks like a decent shuffle io for haskell online
 
 {- ScoreCard Functions -}
 
+increaseScore :: Player -> Player
+increaseScore _ = _
+
+decreaseScore :: Player -> Player
+decreaseScore _ = _
+
+scorePlay :: Integer -> [Card] -> Integer
+scorePlay _ _ = _
 
 {- Dealer Functions -}
 
+newDealer :: Dealer 
+newDealer = _
+
+changeDealer :: Dealer -> Dealer
+changeDealer _ = _
+
+addPlayer :: Dealer -> Player -> Dealer
+addPlayer _ _ = _
+
+removePlayer :: Dealer -> Integer -> Dealer
+removePlayer _ _ = _
+
+deal :: Dealer -> Integer -> Dealer
+deal _ _ = _
+
+discardD :: Dealer -> [Card] -> Dealer
+discard _ _ = _
 
 {- Player Functions -}
+
+newPlayer :: Player
+newPlayer = _
+
+fold :: Dealer -> Integer -> Player
+fold _ _ = _
+
+draw :: Dealer -> Integer -> Dealer
+draw _ _ = _
+
+discardP :: Dealer -> [Card] -> Dealer
+discard _ _ = _
+
+
