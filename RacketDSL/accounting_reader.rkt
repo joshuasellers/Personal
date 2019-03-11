7a88a3196bdd024d285eb3c0ac90216f187a5265
Josh Sellers <joshuasellersd@gmail.com>
2019-03-11 15:43:49 -0400

    finished parser tutorial

--- RacketDSL/accounting_reader.rkt
+++ RacketDSL/accounting_reader.rkt
@@ -3,7 +3,7 @@
 
 (define (read-syntax path port)
   (define parse-tree (parse path (make-tokenizer port)))
-  (define module-datum `(module accounting-mod "accounting_expander.rkt"
+  (define module-datum `(module bf-mod "accounting_expander.rkt"
                           ,parse-tree))
   (datum->syntax #f module-datum))
 (provide read-syntax)
@@ -11,12 +11,11 @@
 (require brag/support)
 (define (make-tokenizer port)
   (define (next-token)
-    (define ac-lexer
+    (define bf-lexer
       (lexer
-       [(char-set "/()yo") lexeme]
-       [numeric lexeme]
+       [(char-set "><-.,+[]") lexeme]
        [any-char (next-token)]))
-    (ac-lexer port))  
+    (bf-lexer port))  
   next-token)
 
 ;  "accounting_expander.rkt"
--- RacketDSL/parser.rkt
+++ RacketDSL/parser.rkt
@@ -1,8 +1,5 @@
 #lang brag
 
-entry :(digit | letter)*
-
-letter : "a" | "A" | "b" | "B"
-digit  : "0" | "1" | "2" | "3" | "4"
-       | "5" | "6" | "7" | "8" | "9"
- 
+bf-program : (bf-op | bf-loop)*
+bf-op      : ">" | "<" | "+" | "-" | "." | ","
+bf-loop    : "[" (bf-op | bf-loop)* "]"
