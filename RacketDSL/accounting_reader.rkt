f6daee628ad3fb85c0346bcb48b2b860676aad67
Josh Sellers <joshuasellersd@gmail.com>
2019-03-12 00:31:12 -0400

    stuff

--- RacketDSL/accounting_reader.rkt
+++ RacketDSL/accounting_reader.rkt
@@ -3,7 +3,7 @@
 
 (define (read-syntax path port)
   (define parse-tree (parse path (make-tokenizer port)))
-  (define module-datum `(module bf-mod "accounting_expander.rkt"
+  (define module-datum `(module ac-mod "accounting_expander.rkt"
                           ,parse-tree))
   (datum->syntax #f module-datum))
 (provide read-syntax)
@@ -11,12 +11,10 @@
 (require brag/support)
 (define (make-tokenizer port)
   (define (next-token)
-    (define bf-lexer
+    (define ac-lexer
       (lexer
-       [(char-set "><-.,+[]") lexeme]
+       [(char-set ",-<>aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ1234567890") lexeme]
        [any-char (next-token)]))
-    (bf-lexer port))  
+    (ac-lexer port))  
   next-token)
 
-;  "accounting_expander.rkt"
-; (equipment 20000) (cash 10000, notes payable 10000)
---
+++ RacketDSL/extra-test.rkt
@@ -0,0 +1,4 @@
+#lang br
+(require "parser.rkt")
+
+(parse-to-datum "01-02-1920<equipment-20000><cash-10000,stock-10000>")
