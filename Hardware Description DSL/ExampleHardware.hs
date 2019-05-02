8d11b27aac300ea5706caa3ae6cfdaa735519e02
Josh Sellers <joshuasellersd@gmail.com>
2019-05-01 20:18:03 -0400

    example

--- Hardware Description DSL/ExampleHardware.hs
+++ Hardware Description DSL/ExampleHardware.hs
@@ -16,7 +16,17 @@ Useful link: https://minnie.tuhs.org/CompArch/Tutes/week02.html
 Useful link: http://web.cse.ohio-state.edu/~teodorescu.1/download/teaching/cse675.au08/Cse675.02.F.ALUDesign_part1.pdf
 -}
 
+alu_1bit :: Bit -> Bit -> Control -> Bit -> (Bit, Bit)
+alu_1bit a b control carry = (result , c)
+    where and = and_gate [a,b]
+          or = or_gate [a,b]
+          add = full_adder a b carry
+          sub = full_adder a (not_gate b) carry
+          ops = [or,and, (fst add),(fst sub)]
+          result = multiplexer (controlVal control) ops
+          c = if (bitVal (control_1 control)) == 1 && (bitVal (control_0 control)) == 1 
+          	     then snd sub
+          	     else snd add
+
 alu :: Bit_32 -> Bit_32 -> Control -> (Bit_32, Bit, Bit, Bit)
-alu a b control = case (bitVal (control_0 control)) of 0 ->
-                                                       1 ->
-            where ands = 
+alu a b control = bitwiseOpp (bit_32Val a) (bit_32Val b) alu_1bit 
