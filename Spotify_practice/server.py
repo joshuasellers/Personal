8b09c5cb0c368792cd6b9d88485fd2a1a81c9033
joshuasellers <joshuasellersd@gmail.com>
2019-07-04 14:05:53 -0400

    updated code for new token

--- Spotify_practice/server.py
+++ Spotify_practice/server.py
@@ -1,36 +1,22 @@
-import http.server
-import socketserver
 import spotipy
 
-import sys
-import spotipy
-import spotipy.util as util
+# SPOTIPY DOESN'T SEEM TO WORK
+#
+# scope = 'user-library-read'
+#username = sys.argv[1]
+# c_id = sys.argv[2]
+# c_secret = sys.argv[3]
+# redirect = 'https://example.com/'# 'https://127.0.0.1:8080/index.html'
+# token = util.prompt_for_user_token(username, scope,c_id,c_secret,redirect)
 
-# SET UP LOCALHOST
 
-PORT = 8000
-Handler = http.server.SimpleHTTPRequestHandler
+token = 'BQDu_57MQ-KT8Qu03lZIFppbqy-Z5wxJyhG9W-VVfvHq0jlcN1gRmYAluMIoaZ90TqyvITqxHhy-KvgT65Onl66f2X5G5LpASGLAjbGI7fu_hUQVdp9mpQKMexO-iIyLvaPbCji-aYutafHVfckQF2C_r5WTC6ucC5A9k-vF9WPCon5yPIxY8_O6ulgdTXBaGeesKCov2srTbWD2PwWGn1w00HwCne4nBzYIBE2WrC28OJyulRY9yNR-8ENbJcVTSQ4mIAEl-Eh5yOg4qhSVlFmiPILAsZTV1ug9MEg'
+if token:
+    sp = spotipy.Spotify(auth=token)
+    results = sp.current_user_saved_tracks()
+    for item in results['items']:
+        track = item['track']
+        print (track['name'] + ' - ' + track['artists'][0]['name'])
+else:
+    print ("Can't get token")
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-
-    # CALL TO SPOTIFY API
-
-    scope = 'user-library-read'
-    username = sys.argv[1]
-    c_id = sys.argv[2]
-    c_secret = sys.argv[3]
-    redirect = sys.argv[4]
-
-    token = util.prompt_for_user_token(username, scope,c_id,c_secret,redirect)
-
-    if token:
-        sp = spotipy.Spotify(auth=token)
-        results = sp.current_user_saved_tracks()
-        for item in results['items']:
-            track = item['track']
-            print (track['name'] + ' - ' + track['artists'][0]['name'])
-    else:
-        print ("Can't get token for", username)
-
-    #httpd.serve_forever()
