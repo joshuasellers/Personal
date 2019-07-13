3465611dbb395f0d19c53c37497d0b8ca75114f9
Josh Sellers <joshuasellersd@gmail.com>
2019-07-12 21:01:15 -0400

    added config file to show how to hide the token and updated the server.py file to get my playlists

--- Spotify_practice/server.py
+++ Spotify_practice/server.py
@@ -1,22 +1,20 @@
 import spotipy
+from config import token
 
-# SPOTIPY DOESN'T SEEM TO WORK
-#
-# scope = 'user-library-read'
-#username = sys.argv[1]
-# c_id = sys.argv[2]
-# c_secret = sys.argv[3]
-# redirect = 'https://example.com/'# 'https://127.0.0.1:8080/index.html'
-# token = util.prompt_for_user_token(username, scope,c_id,c_secret,redirect)
-
-
-token = # Copy from the online console
 if token:
     sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks()
-    for item in results['items']:
-        track = item['track']
-        print (track['name'] + ' - ' + track['artists'][0]['name'])
+    results = []
+    empty = False
+    offset = 0
+    while not empty:
+        result = sp.current_user_playlists(limit=50, offset=offset)
+        offset += 50
+        if len(result['items']) == 0: empty = True
+        else: results.append(result)
+    print(results)
+    for result in results:
+        for item in result['items']:
+            print(item['name'])
 else:
     print ("Can't get token")
 
