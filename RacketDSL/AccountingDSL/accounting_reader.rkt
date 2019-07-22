e30df972c711179dc2ce9e5950f0f32401bc297f
Josh Sellers <joshuasellersd@gmail.com>
2019-07-21 21:02:36 -0400

    editing repo

--- RacketDSL/AccountingDSL/accounting_reader.rkt
+++
@@ -1,7 +0,0 @@
-#lang br/quicklang
-(require "tokenizer.rkt" "parser.rkt")
-
-(define (read-syntax path port)
-  (datum->syntax #f `(module ac-mod "accounting_expander.rkt"
-                       ,(parse path (make-tokenizer port)))))
-(provide read-syntax)
--- RacketDSL/AccountingDSL/tokenizer.rkt
+++
@@ -1,8 +0,0 @@
-#lang br
-(require "lexer.rkt" brag/support)
-
-(define (make-tokenizer port)
-  (define (next-token) (basic-lexer port))
-  next-token)
-
-(provide make-tokenizer)
